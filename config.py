e0509ce734676125c8cc798c42df952b7aaa3c8e
DorcasWanjiku <smallwanjiku@gmail.com>
2020-11-05 19:10:45 +0300

    changed database

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:w1nj3k4@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:w1nj3k4@localhost/blogs'
     SECRET_KEY =os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
    
@@ -13,7 +13,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:w1nj3k4@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:w1nj3k4@localhost/blogs'
     
 
 
@@ -22,7 +22,7 @@ class TestConfig(Config):
     DEBUG = True
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:w1nj3k4@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:w1nj3k4@localhost/blogs'
     DEBUG = True
 
 config_options = {
